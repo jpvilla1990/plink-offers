5cb08824dca9c3615d2a6f5d9a59cc8443751d96
Maximiliano Paz <maximiliano.paz@wolox.com.ar>
2018-07-10 17:28:10 -0300

    fix mails

--- app/services/mailer.js
+++ app/services/mailer.js
@@ -49,17 +49,18 @@ exports.sendNewOffer = (offer, mail, name = null) => {
   return requestService.retail(`/points/${offer.retail}`).then(rv => {
     const postIds = new Array();
     rv.posTerminals.map(value => postIds.push(value.posId));
-    offer.retailName = rv.commerce.description;
-    offer.retailAddres = rv.address;
-    offer.name = name != null ? name : '';
-    offer.nameCategory = offer.nameCategory.toUpperCase();
+    const offerValues = offer.dataValues;
+    offerValues.retailName = rv.commerce.description;
+    offerValues.retailAddres = rv.address;
+    offerValues.name = name != null ? name : '';
+    offerValues.nameCategory = offer.nameCategory.toUpperCase();
     const subjectEmail =
       name != null
         ? i18n.t(`newOffer.subject`)
         : `IdOferta=${offer.id} Nit=${rv.commerce.nit} Posids=${postIds.join()}`;
     const email = {
       subject: subjectEmail,
-      html: servicesHtml.newOffer(offer, mail),
+      html: servicesHtml.newOffer(offerValues, mail),
       to: mail
     };
     return exports.sendEmail(email);
