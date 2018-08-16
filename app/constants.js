b0b05e298379c5b7d1456de2b8a3072fcc0b2cab
Julian Molina <julian.molina@wolox.com.ar>
2018-08-16 20:51:42 +0000

    wording

--- app/constants.js
+++ app/constants.js
@@ -1,9 +1,8 @@
 exports.OFFER_ACTIVE = 'active';
 exports.OFFER_INACTIVE = 'inactive';
-exports.OFFER_DISABLED = 'disable';
+exports.OFFER_DISABLED = 'disabled';
 exports.NEW_OFFER = 'newOffer';
 exports.OFFER_EXPIRED = 'offerExpired';
 exports.NEW_CODE = 'newCode';
 exports.BEFORE_BEGIN = 'offerBeforeBegin';
-exports.MAX_LENGTH_OFFER_DETAIL = 17;
 exports.HIDE_EMAIL = 3;
